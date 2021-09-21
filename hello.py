72db500ae8bfe3c1c72a7235d2b53e9ecfd14511
sgering <seth.gering@gmail.com>
2021-09-20 18:59:01 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import webbrowser
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates"))
 
 @app.route("/")
 def hello_world():
@@ -10,4 +10,4 @@ def hello_world():
     webbrowser.open_new_tab('http://usgs.gov')  # Go to example.com
     webbrowser.open_new_tab('https://flask.palletsprojects.com/en/2.0.x/quickstart/')  # Go to example.com
     
-    return webbrowser.open_new_tab('http://usgs.gov')
+    return "<p>Hello, World!</p>"
