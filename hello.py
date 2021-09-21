3c83cb2f3e028fa19b8d9ad77ba4d2e30361a81c
sgering <seth.gering@gmail.com>
2021-09-20 18:59:16 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import webbrowser
 
-app = Flask(__name__,template_folder="templates"))
+app = Flask(__name__,template_folder="templates")
 
 @app.route("/")
 def hello_world():
