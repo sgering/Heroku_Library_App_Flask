cd08c4d2e01fd620916bfb311d97459dc7e1c7e7
sgering <seth.gering@gmail.com>
2021-09-20 19:20:16 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,17 +1,7 @@
-from flask import Flask
-import os
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  
-  os.system("open https://google.com")
-  return "Hello World!"
-
-@app.route("/John")
-def John():
-  return <a href="http://mylink.com" target="_blank">Link</a>
-
-if __name__ == "__main__":
-  app.run(debug=True)
+@app.route('/')
+def home():
+    return render_template('home.html')
+if __name__ == '__main__':
+    app.run(debug=True)
