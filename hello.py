01b44ed45e757cd17c3ad3a2b6e4f9cfba866d71
sgering <seth.gering@gmail.com>
2021-09-20 18:27:20 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,4 +10,4 @@ def hello_world():
     webbrowser.open_new_tab('http://usgs.gov')  # Go to example.com
     webbrowser.open_new_tab('https://flask.palletsprojects.com/en/2.0.x/quickstart/')  # Go to example.com
     
-    return "<p>Hello, World!</p>"
+    return webbrowser.open_new_tab('http://usgs.gov')
